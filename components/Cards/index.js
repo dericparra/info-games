9e9404cb2cff4e31c5db2b2b71a47e52ef58b7cd
dericparra <parradeko@gmail.com>
2022-01-13 17:17:05 -0300

    [spike] Adcionar Link nos cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import { Container, GameTitle } from './style'
 
 const Cards = () => {
   return (
-    <Container>
-      <Image src="/images/cardsImg.jpg" width={80} height={80} alt="Game" />
-      <GameTitle>Jogo bolado de ps1</GameTitle>
-    </Container>
+    <Link href="/gamePage">
+      <Container>
+        <Image src="/images/cardsImg.jpg" width={80} height={80} alt="Game" />
+        <GameTitle>Jogo bolado de ps1</GameTitle>
+      </Container>
+    </Link>
   )
 }
 
